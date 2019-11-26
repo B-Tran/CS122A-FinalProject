4d861d3de4ce7a9bb0395995f0a472b2afd48015
Angel Renteria <arent007@ucr.edu>
2019-11-26 04:07:35 -0800

    Made more examples for flask server
    -bugs: cannot send over json to the html client
    - need to find way to synchronize updates
    - flask hangs when server is sending stream of data

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -1,32 +1,47 @@
 from flask import Flask, render_template
 from flask import Response
 from flask import jsonify
+from flask import json
+from flask import make_response
 
 app = Flask(__name__)
 increment = 0
 
+#List_Data= {"data1":"1", "data2":"2", "data3":"3", "data4":"4"}
+List_Data = ["1","2","3","4"]
 @app.route('/')
 def index():
-    #my_list = ['slammin salmon', 'sprite fizzy', 'slammy fenderbender', 'SUCC', 'sock fapper']
-    #return render_template('index.html', my_list = my_list)
-    return render_template('test.html')
-    #return 'This is my flask website and it is very bad.'
+    return render_template('index.html')
+    #return 'this is a test.'
 
-@app.route('/hewwo')
-def hewwo():
-    return render_template('hewwo.html')
-    #return 'HEWWO, TES ES TE FBI.'
+@app.route('/update')
+def update():
+    List_Data[0] = List_Data[1]
+    List_Data[1] = List_Data[2]
+    List_Data[2] = List_Data[3]
+    List_Data[3] = "%d" % (int(List_Data[3])+1)
+    return None
 
-@app.route('/hewwo2')
-def hewwo2():
-    global increment
-    increment += 1
-    if(increment%3 == 0):
-        response = "data:HEWWO, TES ES TE FBI. %d\n\n" %(increment)
-        return Response( response , mimetype="text/event-stream")
-    else:
-        response = ""
-        return Response( response , mimetype="text/event-stream")
+@app.route('/GetData1')
+def GetData1():
+    sendData = "data: %s\n\n" %(List_Data[0])
+    return Response(sendData, mimetype="text/event-stream")
+    #return Response("data:hello.\n\n", mimetype="text/event-stream")
+
+@app.route('/GetData2')
+def GetData2():
+    sendData = "data: %s\n\n" %(List_Data[1])
+    return Response(sendData, mimetype="text/event-stream")
+
+@app.route('/GetData3')
+def GetData3():
+    sendData = "data: %s\n\n" %(List_Data[2])
+    return Response(sendData, mimetype="text/event-stream")
+
+@app.route('/GetData4')
+def GetData4():
+    sendData = "data: %s\n\n" %(List_Data[3])
+    return Response(sendData, mimetype="text/event-stream")
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
