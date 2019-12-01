f289fb99bcdbe943c047b63ba24e4e8bf11f5bd6
B-Tran <btran054@ucr.edu>
2019-12-01 08:52:03 -0800

    Modified the Flask Server to send JSON to Client
    - Fixed list synchronization
    - Bug: fix the printing out of the list

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask import Response
 from flask import jsonify
 from flask import json
+#import simplejson
 from flask import make_response
 
 app = Flask(__name__)
@@ -22,26 +23,32 @@ def update(SensorData):
     List_Data[0] = "%s" % (SensorData)
     return jsonify(List_Data)
 
+def GenerateList():
+    json_data = json.dumps(
+            {'data1':List_Data[0] , 'data2':List_Data[1] , 'data3':List_Data[2], 'data4':List_Data[3]})
+    yield f"data:{json_data}\n\n"
+
 @app.route('/GetData1')
 def GetData1():
-    sendData = "data: %s\n\n" %(List_Data[0])
-    return Response(sendData, mimetype="text/event-stream")
+    return Response(GenerateList(), mimetype='text/event-stream')
+    #sendData = "data: %s\n\n" %(List_Data[0])
+    #return Response(sendData, mimetype="text/event-stream")
     #return Response("data:hello.\n\n", mimetype="text/event-stream")
 
-@app.route('/GetData2')
-def GetData2():
-    sendData = "data: %s\n\n" %(List_Data[1])
-    return Response(sendData, mimetype="text/event-stream")
+#@app.route('/GetData2')
+#def GetData2():
+#    sendData = "data: %s\n\n" %(List_Data[1])
+#    return Response(sendData, mimetype="text/event-stream")
 
-@app.route('/GetData3')
-def GetData3():
-    sendData = "data: %s\n\n" %(List_Data[2])
-    return Response(sendData, mimetype="text/event-stream")
+#@app.route('/GetData3')
+#def GetData3():
+#    sendData = "data: %s\n\n" %(List_Data[2])
+#    return Response(sendData, mimetype="text/event-stream")
 
-@app.route('/GetData4')
-def GetData4():
-    sendData = "data: %s\n\n" %(List_Data[3])
-    return Response(sendData, mimetype="text/event-stream")
+#@app.route('/GetData4')
+#def GetData4():
+#    sendData = "data: %s\n\n" %(List_Data[3])
+#    return Response(sendData, mimetype="text/event-stream")
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
